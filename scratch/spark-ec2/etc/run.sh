a4832fbc6ea02ca5826a822e920346b8d3b1bce9
root <root@ip-172-31-23-74.us-west-1.compute.internal>
2015-03-09 23:49:17 +0000

    disable the CHECKIMAGE_TYPE option in sextractor

--- scratch/spark-ec2/etc/run.sh
+++ scratch/spark-ec2/etc/run.sh
@@ -6,7 +6,7 @@ i=0
 pids=""
 for f in `ls /mnt/input_18_18`
 do
-  sex /mnt/input_18_18/$f -CATALOG_NAME catalog_18_18/$f.cat &
+  sex /mnt/input_18_18/$f -CATALOG_NAME catalog_18_18/$f.cat -CHECKIMAGE_TYPE NONE &
   pids="$pids $!"
   i=$((i+1))
   if [ "${i}" -eq "${proc}" ];
@@ -16,3 +16,4 @@ do
       i=0
   fi
 done
+wait
