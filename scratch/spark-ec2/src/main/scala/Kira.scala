9a288b19889b1701807f13c0aa7f8f7628eef78a
root <root@ip-172-31-18-50.us-west-1.compute.internal>
2015-03-20 02:06:52 +0000

    fix the POSIX file interface

--- scratch/spark-ec2/src/main/scala/Kira.scala
+++ scratch/spark-ec2/src/main/scala/Kira.scala
@@ -17,16 +17,16 @@ object Kira {
     val flatresults = results.flatMap(p => p.map(r => (r._1, r._2, r._3, r._4, r._5, r._6, r._7, r._8, r._9)))
     flatresults.saveAsTextFile(args(1))
 
-    /*val flist = new File(src).listFiles.filter(_.getName.endsWith(".fits"))
+    /*val flist = new File(src).listFiles.filter(_.getName.endsWith(".fit"))
 
     val dflist = sc.parallelize(flist)
     val results = dflist.map(f => extract(f.toString))
 
-    val flatresults = results.flatMap(p => p.map(r => (r._1, r._2, r._3, r._4, r._5)))
-    println("count: " + flatresults.count)
-    flatresults.saveAsTextFile("output")*/
+    val flatresults = results.flatMap(p => p.map(r => (r._1, r._2, r._3, r._4, r._5, r._6, r._7, r._8, r._9)))
+    //println("count: " + flatresults.count)
+    flatresults.saveAsTextFile(args(1))*/
   }
-  def extract(path: String): Array[(Double, Double, Double, Double, Short)] = {
+  def extract(path: String): Array[(Int, Double, Double, Double, Double, Double, Double, Double, Short)] = {
     var matrix = Utils.load(path)
     var bkg = new Background(matrix)
     matrix = bkg.subfrom(matrix)
@@ -35,15 +35,28 @@ object Kira {
     //return objects.length
     var x: Array[Double] = Array.ofDim[Double](objects.length)
     var y: Array[Double] = Array.ofDim[Double](objects.length)
+    var a: Array[Double] = Array.ofDim[Double](objects.length)
+    var b: Array[Double] = Array.ofDim[Double](objects.length)
+    var theta: Array[Double] = Array.ofDim[Double](objects.length)
+    var r: Array[Double] = Array.fill(objects.length) { 6.0 }
     for (i <- (0 until objects.length)) {
       x(i) = objects(i).x
       y(i) = objects(i).y
+      a(i) = objects(i).a
+      b(i) = objects(i).b
+      theta(i) = objects(i).theta
     }
 
     var err: Array[Array[Double]] = Array.fill(matrix.length, matrix(0).length) { bkg.bkgmap.globalrms }
 
     val (flux, fluxerr, flag) = ex.sum_circle(matrix, x, y, 5.0, err = err)
-    val retArray = (0 until objects.length).map(i => (x(i), y(i), flux(i), fluxerr(i), flag(i))).toArray
+    val (kr, flag2) = ex.kron_radius(matrix, x, y, a, b, theta, r)
+    val kr_ellipse = kr.map(x => 2.5 * x)
+
+    val (flux_auto, flux_auto_err, auto_flag) = ex.sum_ellipse(matrix, x, y, a, b, theta, kr_ellipse, err = err, subpix = 1)
+
+    val retArray = (0 until objects.length).map(i => (i, x(i), y(i), flux(i), fluxerr(i), kr(i), flux_auto(i), flux_auto_err(i), auto_flag(i))).toArray
+
     return retArray
   }
 
